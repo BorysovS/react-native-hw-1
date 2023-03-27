9f9433fd79186812e2917ab2b0c2d0d2d1be140d
BorysovS <s.borisovbar@gmail.com>
2023-03-28 01:27:15 +0300

    add logic

--- myNewRnProject/Screens/RegistrationScreen.jsx
+++ myNewRnProject/Screens/RegistrationScreen.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -5,46 +6,124 @@ import {
   TextInput,
   TouchableOpacity,
   KeyboardAvoidingView,
-    Platform,
+  Platform,
   TouchableWithoutFeedback,
-  Keyboard,
+    Keyboard,
+  useWindowDimensions
 } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+const initialState = {
+  userLogin: '',
+  userMail: '',
+  userPassword: '',
+};
 
 export default function RegistrationScreen() {
-    return (
-        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    >
-      <View style={styles.regBox}>
-        <View style={styles.form}>
-          <Text style={styles.formTitle}>Registration</Text>
-          <View style={{ marginBottom: 16 }}>
-            <TextInput style={styles.input} placeholder="Login" placeholderTextColor='#BDBDBD'/>
-          </View>
-          <View style={{ marginBottom: 16 }}>
-            <TextInput style={styles.input} placeholder="E-mail" placeholderTextColor='#BDBDBD'/>
-          </View>
-          <View style={{ marginBottom: 43 }}>
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-                          secureTextEntry={true}
-                          placeholderTextColor='#BDBDBD'
-            />
-          </View>
-          <View style={{ marginBottom: 16 }}>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.btnText}>Sign Up</Text>
-            </TouchableOpacity>
-          </View>
-                  <View style={{alignItems: 'center'}}>
-            <Text>Already have an account? Log in</Text>
+  const [isFocused, setIsFocused] = useState(false);
+    const [dataUserState, setDataUserState] = useState(initialState);
+    const { height, width } = useWindowDimensions();
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+    
+//     useEffect(() => {
+//         const onChange = () => { 
+//             const width = Dimensions.get('window').width;
+//         }
+//    }, [])
+    
+
+  const keyboardHide = () => {
+    setIsFocused(false);
+    Keyboard.dismiss();
+    console.log(dataUserState);
+    setDataUserState(initialState);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View
+          style={{ ...styles.regBox, paddingBottom: isFocused ? 32 : 92, paddingBottom: height <= 420 ? 8 : 45, }}
+          onLayout={onLayoutRootView}
+        >
+          <View style={styles.form}>
+            <Text style={styles.formTitle}>Registration</Text>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                style={styles.input}
+                placeholder="Login"
+                placeholderTextColor="#BDBDBD"
+                onFocus={() => setIsFocused(true)}
+                onChangeText={value =>
+                  setDataUserState(pervState => ({
+                    ...pervState,
+                    userLogin: value,
+                  }))
+                }
+                value={dataUserState.userLogin}
+              />
+            </View>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                style={styles.input}
+                placeholder="E-mail"
+                placeholderTextColor="#BDBDBD"
+                onFocus={() => setIsFocused(true)}
+                onChangeText={value =>
+                  setDataUserState(pervState => ({
+                    ...pervState,
+                    userMail: value,
+                  }))
+                }
+                value={dataUserState.userMail}
+              />
+            </View>
+            <View style={{ marginBottom: 43 }}>
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry={true}
+                placeholderTextColor="#BDBDBD"
+                onFocus={() => setIsFocused(true)}
+                onChangeText={value =>
+                  setDataUserState(pervState => ({
+                    ...pervState,
+                    userPassword: value,
+                  }))
+                }
+                value={dataUserState.userPassword}
+              />
+            </View>
+            <View style={{ marginBottom: 16 }}>
+              <TouchableOpacity style={styles.button} onPress={keyboardHide}>
+                <Text style={styles.btnText}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <Text>Already have an account? Log in</Text>
+            </View>
           </View>
         </View>
-      </View>
-            </KeyboardAvoidingView>
-            </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -64,7 +143,8 @@ const styles = StyleSheet.create({
   },
 
   formTitle: {
-    color: '#212121',
+      color: '#212121',
+      fontFamily: 'Roboto-Medium',
     fontWeight: '500',
     fontSize: 30,
     textAlign: 'center',
@@ -73,18 +153,15 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    backgroundColor: '#F6F6F6',
-    padding: 16,
-    borderRadius: 8,
     borderWidth: 1,
-    borderColor: '#E8E8E8',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    color: '#212121',
-    fontStyle: 'normal',
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    padding: 16,
+    fontFamily: "Roboto-Regular",
+    fontWeight: "400",
     fontSize: 16,
-    lineHeight: 19,
-    minWidth: 343,
   },
 
   button: {
@@ -97,7 +174,8 @@ const styles = StyleSheet.create({
 
   btnText: {
     fontSize: 16,
-    fontWeight: '400',
+      fontWeight: '400',
+    fontFamily: 'Roboto-Regular',
     color: '#FFFFFF',
     lineHeight: 19,
   },
